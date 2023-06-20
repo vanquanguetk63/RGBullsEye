63b2649fb7b7618df58f73a8fb04685a0eefc518
quang.tran41 <quang.tran4@mservice.com.vn>
2023-06-20 14:49:17 +0700

    first commit

--- RGBullsEye/ContentView.swift
+++ RGBullsEye/ContentView.swift
@@ -8,19 +8,74 @@
 import SwiftUI
 
 struct ContentView: View {
+    let rTarget = Double.random(in: 0..<1)
+    let gTarget = Double.random(in: 0..<1)
+    let bTarget = Double.random(in: 0..<1)
+    @State var rGuess: Double
+    @State var gGuess: Double
+    @State var bGuess: Double
+    @State var showAlert = false
+    
+    func computeScore() -> Int {
+        let rDiff = rGuess - rTarget
+        let gDiff = gGuess - gTarget
+        let bDiff = bGuess - bTarget
+        let diff = sqrt(rDiff * rDiff + gDiff * gDiff + bDiff * bDiff)
+        return Int((1 - diff) * 100.0 + 0.5)
+    }
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            HStack {
+              // Target color block
+              VStack {
+                  Rectangle().foregroundColor(Color(red: rTarget,green: gTarget, blue: bTarget, opacity: 1))
+                Text("Match this color")
+              }
+              // Guess color block
+              VStack {
+                  Rectangle().foregroundColor(Color(red: rGuess,green: gGuess, blue: bGuess, opacity: 1))
+                  HStack {
+                      Text("R: \(Int(rGuess * 255.0))")
+                      Text("G: \(Int(gGuess * 255.0))")
+                      Text("B: \(Int(bGuess * 255.0))")
+                }
+              }
+            }
+            
+            ColorChangeView(value: $rGuess,textColor: .red)
+            ColorChangeView(value: $gGuess, textColor: .green)
+            ColorChangeView(value: $bGuess,textColor:  .blue)
+            Button(action: {
+                self.showAlert = true
+            }) {
+                Text("Hit Me!")
+            }
+            .alert(isPresented: $showAlert) {
+                Alert(title: Text("Your Score"), message: Text("\(computeScore())"))
+            }
+
         }
-        .padding()
+
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        ContentView(rGuess: 0.5, gGuess: 0.5, bGuess: 0.5)
+    }
+}
+
+struct ColorChangeView: View {
+    @Binding var value: Double
+    var textColor: Color
+    var body: some View {
+        HStack {
+            Text("0")
+                .foregroundColor(textColor)
+            Slider(value: $value)
+            Text("255")
+                .foregroundColor(textColor)
+        }.padding()
     }
 }
