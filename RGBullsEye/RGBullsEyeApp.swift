dcd8bc81aa856cad9c1080ea5bdb69b9e5b4d91a
quang.tran41 <quang.tran4@mservice.com.vn>
2023-06-20 13:56:06 +0700

    Initial Commit

---
+++ RGBullsEye/RGBullsEyeApp.swift
@@ -0,0 +1,17 @@
+//
+//  RGBullsEyeApp.swift
+//  RGBullsEye
+//
+//  Created by quang.tran4 on 20/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct RGBullsEyeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
